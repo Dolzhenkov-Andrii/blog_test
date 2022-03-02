d666cfbf054260c5c4fc3eafbfdf5d5294dc9b3f
Dolzhenkov-Andrii <oscurabow@gmail.com>
2022-03-02 23:07:39 +0200

    com[2] add new file 2

--- README.md
+++
@@ -1,4 +0,0 @@
-# Blog_UsPython
-My first repository
-I... i do not understand...
-This is my first attempt at working with the repository... =_=
---
+++ python_t1.py
@@ -0,0 +1,11 @@
+from turtle import end_poly
+
+
+u_surname = "Dolzhenkov"
+u_name = "Andrii"
+u_sex = "M"
+u_age = 28
+print("HELLO!!!")
+print("Name: " + u_surname + " " + u_name)
+print("sex: " + u_sex + " age: " + str(u_age))
+
