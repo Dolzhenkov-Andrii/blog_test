52a9d8e38dfe556661cbdce0f27c8ca6beee453c
Dolzhenkov-Andrii <oscurabow@gmail.com>
2022-03-03 19:01:08 +0200

    com[6]

--- test_1/python_t1.py
+++ test_1/python_t1.py
@@ -4,7 +4,7 @@ from turtle import end_poly
 u_surname = "Dolzhenkov"
 u_name = "Andrii"
 u_sex = "M"
-u_age = 28
+u_age = 29
 print("HELLO!!!")
 print("Name: " + u_surname + " " + u_name)
 print("sex: " + u_sex + " age: " + str(u_age))
